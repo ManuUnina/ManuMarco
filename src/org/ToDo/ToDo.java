e7d0cb75dd8cad34b14d85866d92acacd2e34ea5
ManuUnina <emanu.palmieri@studenti.unina.it>
2025-04-15 10:02:25 +0200

    Primo commento

---
+++ src/org/ToDo/ToDo.java
@@ -0,0 +1,18 @@
+package org.ToDo;
+
+/*import java.sql.Blob;
+import java.util.Date;
+
+public class ToDo{
+    public String titolo;
+    public Date scadenza;
+    public String posizione;
+    public String url;
+    public String descrizione;
+    public Blob immagine;
+    public Boolean stato;
+
+
+
+}
+*/
