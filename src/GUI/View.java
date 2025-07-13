29c54628bb0ccb0e23c9de3c4b73ecf4ba41e1eb
ManuUnina <emanu.palmieri@studenti.unina.it>
2025-07-13 22:47:52 +0200

    Versione 2.0 con aggiunta delle interfaccie per il DAO

--- src/GUI/View.java
+++ src/GUI/View.java
@@ -19,7 +19,6 @@ import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
